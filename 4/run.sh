5286de5d77ba7e2d22fd0cca8d40c785881a8a69
titi-programmer <tgfein@gmail.com>
2020-08-04 19:17:58 -0300

    hihi

--- 4/run.sh
+++ 4/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run -ti --rm \
+docker run --name titi -ti --rm \
     -v "$PWD/../1/linux":/home/student/src/linux \
     -v "$PWD":/home/student/src/initramfs \
     -v "$PWD/../initramfs_base":/home/student/src/initramfs_base \
